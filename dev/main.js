b8e3d834b30a17cd8748e8f781e90480e5b1b073
Okan Atabağ <okanatabag@gmail.com>
2016-10-18 01:12:57 +0300

    self commit

--- dev/main.js
+++ dev/main.js
@@ -2,73 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(){
-      super();
-       this.state = {
-           data: []
-       };
-       this.addTodo = this.addTodo.bind(this);
-       this.getData = this.getData.bind(this);
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            update:0
+        };
     }
-    addTodo(e){
-      fetch("http://localhost:8000/todo", {
-        mode: 'cors',
-        method: "post",
-        headers: {
-         'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
-         'Content-Type': 'application/json'
-        },
-        body:JSON.stringify({
-            name:ReactDOM.findDOMNode(this.refs.new_todo_input.refs.new_todo).value,
-        })
-      })
-      .then(function(res){
+    componentWillMount() {
         this.getData();
-      })
     }
-    getData(){
-      fetch("http://localhost:8000/todo")
-          .then( (response) => {
-              return response.json() })
-          .then( (json) => {
-              this.setState({data: json});
-          });
+    addTodo() {
+        var self = this;
+        fetch('http://localhost:8000/todo', {
+            mode: 'cors',
+            method: 'post',
+            headers: {
+                'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: ReactDOM.findDOMNode(this.refs.input).value
+            })
+        }).then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            self.getData();
+        });
     }
-    componentDidMount() {
-      this.getData();
-    }
+    getData() {
+        var self = this;
+        fetch('http://localhost:8000/todo').then((response) => {
+            return response.json();
+        }).then((json) => {
+            self.setState({data: json});
+        });
+    };
     render() {
-        return (<div>
-            <ul>
-              {this.state.data.map(function(value){
-                return <TodoListItem name={value.name} />
-              })}
-            </ul>
-            <TodoAddElements ref="new_todo_input" event={this.addTodo} />
-          </div>
+        return (
+            <div>
+              <input name="name" ref="input" type="text" defaultValue=""/>
+              <button type="button" onClick={this.addTodo.bind(this)}>Ekle</button>
+                <ul>
+                    {this.state.data.map(function(value,i) {
+                        return <TodoListItem key={i} name={value.name}/>
+                    })}
+                </ul>
+            </div>
         );
     }
 }
 
-
-class TodoListItem extends React.Component {
-  render() {
-    return (
-      <li>{this.props.name}</li>
-    );
-  }
+function TodoListItem(props) {
+    return (<li>{props.name}</li>);
 }
 
-class TodoAddElements extends React.Component {
-  render() {
-    return (
-      <div>
-        <input name="name" ref="new_todo" type="text" defaultValue=""/>
-        <button type="button" onClick={this.props.event}>Ekle</button>
-      </div>
-    );
-  }
-
-}
-
-ReactDOM.render(<App /> ,document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
