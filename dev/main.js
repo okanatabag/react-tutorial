98d6121f185bd21244a49dafaee96d2acba687fb
Okan Atabağ <okanatabag@gmail.com>
2016-10-19 01:36:33 +0300

    css-loader added

--- dev/main.js
+++ dev/main.js
@@ -1,6 +1,8 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+require('./main.less');
 class App extends React.Component {
     constructor() {
         super();
@@ -11,6 +13,14 @@ class App extends React.Component {
     componentWillMount() {
         this.getTodoList();
     }
+    getTodoList() {
+        var self = this;
+        fetch('http://localhost:8000/todo').then((response) => {
+            return response.json();
+        }).then((json) => {
+            self.setState({data: json});
+        });
+    };
     addTodo() {
         var self = this;
         fetch('http://localhost:8000/todo', {
@@ -30,14 +40,44 @@ class App extends React.Component {
             self.getTodoList();
         });
     }
-    getTodoList() {
+    toggleTodo(id){
         var self = this;
-        fetch('http://localhost:8000/todo').then((response) => {
+        fetch('http://localhost:8000/todo', {
+            mode: 'cors',
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: id
+            })
+        }).then(function(response) {
             return response.json();
-        }).then((json) => {
-            self.setState({data: json});
-        });
-    };
+        })
+    .then(function(json) {
+        self.getTodoList();
+    });
+    }
+    delTodo(id) {
+        var self = this;
+        fetch('http://localhost:8000/todo', {
+            mode: 'cors',
+            method: 'delete',
+            headers: {
+                'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: id
+            })
+        }).then(function(response) {
+            return response.json();
+        })
+      .then(function(json) {
+          self.getTodoList();
+      });
+    }
     render() {
         var self = this;
         return (
@@ -46,16 +86,19 @@ class App extends React.Component {
               <button type="button" onClick={self.addTodo.bind(this)}>Ekle</button>
                 <ul>
                     {this.state.data.map(function(value,i) {
-                        return <TodoListItem key={i} name={value.name}/>
+                        return <TodoListItem
+                                key={value.id}
+                                name={value.name}
+                                toggleFunc={() =>self.toggleTodo(value.id)}
+                                delFunc={()=>self.delTodo(value.id)}  />
                     })}
                 </ul>
             </div>
         );
     }
 }
-
 function TodoListItem(props) {
-    return (<li>{props.name}</li>);
+    return (<li><a className="text" href="javascript:void(0)" onClick={props.toggleFunc}>{props.name}</a> <a className="del" href="javascript:void(0)" onClick={props.delFunc}><span>&times;</span></a></li>);
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
